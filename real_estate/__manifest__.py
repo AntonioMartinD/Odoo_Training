3ad693f9561ab36bf1a763e3f9c254e6cf77162a
AntonioMartinD <antoniomartin@vauxoo.com>
2023-05-06 21:58:09 -0600

    [ADD] real_state: added the new module

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -1,23 +1,19 @@
-# -*- coding: utf-8 -*-
 {
     'name': "real_estate",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Technical Training Module",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'description': "The real state module is create base on the Odoo 16.0 documentation",
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Vauxoo",
+
+    'license' : "LGPL-3",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '16.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -25,11 +21,10 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        
     ],
+    'application':True
 }
