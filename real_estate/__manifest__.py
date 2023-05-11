15afef0b82360d20a78569b5f017b9824eeb4d15
AntonioMartinD <antoniomartin@vauxoo.com>
2023-05-11 15:38:37 -0600

    [IMP] real_estate: add new field's attributes on property model

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -1,32 +1,19 @@
 {
-    'name': "real_estate",
-
-    'summary': "Technical Training Module",
-
-    'description': "The real state module is create base on the Odoo 16.0 documentation",
-
-    'author': "Vauxoo",
-
-    'license' : "LGPL-3",
-
+    "name": "real_estate",
+    "summary": "Technical Training Module",
+    "description": "The real state module is create base on the Odoo 16.0 documentation",
+    "author": "Vauxoo",
+    "license": "LGPL-3",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '16.0.1.0.0',
-
+    "category": "Uncategorized",
+    "version": "16.0.1.0.0",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
-         'security/ir.model.access.csv',
-         'views/property_views.xml',
-         'data/estate_menus.xml'
-    ],
+    "data": ["security/ir.model.access.csv", "views/property_views.xml", "data/estate_menus.xml"],
     # only loaded in demonstration mode
-    'demo': [
-        
-    ],
-    'application':True
+    "demo": [],
+    "application": True,
 }
--- real_estate/models/property.py
+++ real_estate/models/property.py
@@ -1,25 +1,40 @@
-from odoo import fields,models,api
+from datetime import timedelta
+
+from odoo import fields, models
+
 
 class Property(models.Model):
     _name = "property"
     _description = "Estate Property Model"
-    
-    name = fields.Char("Title", required = True)
+
+    name = fields.Char("Title", required=True)
     description = fields.Text("Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Available From")
-    expected_price = fields.Float("Expected Price", required = True)
-    selling_price = fields.Float ("Selling Price")
-    bedrooms = fields.Integer("Bedrooms")
+    date_availability = fields.Date(
+        "Available From", default=lambda self: fields.Datetime.now() + timedelta(days=90), copy=False
+    )
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price", readonly=True)
+    bedrooms = fields.Integer("Bedrooms", default=2)
     living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
-    garden = fields.Boolean ("Garden")
+    garden = fields.Boolean("Garden")
     garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
-        string = 'Garden Orientation',
-        selection = [('north', 'North'),
-                     ('south', 'South'),
-                     ('east','East'), 
-                     ('weast','Weast')]
+        string="Garden Orientation",
+        selection=[("north", "North"), ("south", "South"), ("east", "East"), ("weast", "Weast")],
     )
+    state = fields.Selection(
+        string="Status",
+        selection=[
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("acepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        copy=False,
+        default="new",
+    )
+    active = fields.Boolean("Active", default=False)
