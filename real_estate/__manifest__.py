33b4499e8284a49953d14cf9565929e50f9f1fee
AntonioMartinD <antoniomartin@vauxoo.com>
2023-05-13 19:05:24 -0600

    [IMP] real_estate: add the properties kanban view usign Qweb

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -4,14 +4,9 @@
     "description": "The real state module is create base on the Odoo 16.0 documentation",
     "author": "Vauxoo",
     "license": "LGPL-3",
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     "category": "Uncategorized",
     "version": "16.0.1.0.0",
-    # any module necessary for this one to work correctly
     "depends": ["base"],
-    # always loaded
     "data": [
         "security/ir.model.access.csv",
         "views/property_views.xml",
@@ -21,7 +16,5 @@
         "views/res_users_views.xml",
         "data/estate_menus.xml",
     ],
-    # only loaded in demonstration mode
-    "demo": [],
     "application": True,
 }
