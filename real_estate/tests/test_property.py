3873a01866216310aaee8af805671320175f40e1
AntonioMartinD <antoniomartin@vauxoo.com>
2023-05-19 15:28:18 -0600

    [ADD] real_estate: add PDF reports

--- real_estate/tests/test_property.py
+++ real_estate/tests/test_property.py
@@ -1,56 +1,51 @@
-from odoo.tests.common import TransactionCase, Form
 from odoo.exceptions import UserError
 from odoo.tests import tagged
+from odoo.tests.common import Form, TransactionCase
 
-@tagged('post_install', '-at_install')
+
+@tagged("post_install", "-at_install")
 class PropertyTestCase(TransactionCase):
-
     @classmethod
     def setUpClass(cls):
-        super(PropertyTestCase, cls).setUpClass()
-        cls.property = cls.env['property'].create({
-            "name" : "Big Villa",
-            "state" : "new",
-            "description" : "A nice and big villa",
-            "postcode" : "12345",
-            "date_availability" : "2023-05-02",
-            "expected_price" : "1600000",
-            "bedrooms" : "6",
-            "living_area" : "100",
-            "facades" : "4",
-            "garage" : "True",
-            "garden" : "True",
-            "garden_area" : "10000",
-            "garden_orientation" : "south",
-            "property_type_id" : "4"
-        })
-    
+        super().setUpClass()
+        cls.property = cls.env["property"].create(
+            {
+                "name": "Big Villa",
+                "state": "new",
+                "description": "A nice and big villa",
+                "postcode": "12345",
+                "date_availability": "2023-05-02",
+                "expected_price": "1600000",
+                "bedrooms": "6",
+                "living_area": "100",
+                "facades": "4",
+                "garage": "True",
+                "garden": "True",
+                "garden_area": "10000",
+                "garden_orientation": "south",
+                "property_type_id": "4",
+            }
+        )
+
     def test_action_sold_no_accepted_offer(self):
-        self.env['property.offer'].create({
-            'property_id': self.property.id,
-            'partner_id': '1',
-            'status': 'refused',
-            'price' : 1600000
-            
-        })
+        self.env["property.offer"].create(
+            {"property_id": self.property.id, "partner_id": "1", "status": "refused", "price": 1600000}
+        )
         with self.assertRaises(UserError) as context:
             self.property.action_sold()
 
         self.assertIn("Property cannot be sold without any accepted offers", str(context.exception))
-        self.assertEqual(self.property.state, 'received')
+        self.assertEqual(self.property.state, "received")
 
     def test_action_sold_accepted_offer(self):
-        self.env['property.offer'].create({
-            'property_id': self.property.id,
-            'partner_id': '1',
-            'status': 'accepted',
-            'price' : 1600000
-        })
+        self.env["property.offer"].create(
+            {"property_id": self.property.id, "partner_id": "1", "status": "accepted", "price": 1600000}
+        )
         self.property.action_sold()
-        self.assertEqual(self.property.state, 'sold')
+        self.assertEqual(self.property.state, "sold")
 
     def test_onchange_garden(self):
-        
+
         form = Form(self.property)
         property_record = form.save()
 
@@ -58,9 +53,8 @@ class PropertyTestCase(TransactionCase):
         property_record._onchange_garden()
         self.assertEqual(property_record.garden_area, 10)
         self.assertEqual(property_record.garden_orientation, "north")
-        
+
         property_record.garden = False
         property_record._onchange_garden()
         self.assertEqual(property_record.garden_area, 0)
         self.assertFalse(property_record.garden_orientation)
-
--- real_estate/tests/test_property_offer.py
+++ real_estate/tests/test_property_offer.py
@@ -1,37 +1,34 @@
-from odoo.tests.common import TransactionCase
-from odoo.exceptions import  ValidationError
+from odoo.exceptions import ValidationError
 from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
 
-@tagged('post_install', '-at_install')
+
+@tagged("post_install", "-at_install")
 class PropertyOfferTestCase(TransactionCase):
-
     @classmethod
     def setUpClass(cls):
-        super(PropertyOfferTestCase, cls).setUpClass()
-        cls.property = cls.env['property'].create({
-            "name" : "Big Villa",
-            "state" : "sold",
-            "description" : "A nice and big villa",
-            "postcode" : 12345,
-            "date_availability" : "2023-05-02",
-            "expected_price" : 1600000,
-            "bedrooms" : 6,
-            "living_area" : "100",
-            "facades" : "4",
-            "garage" : "True",
-            "garden" : "True",
-            "garden_area" : 10000,
-            "garden_orientation" : "south",
-            "property_type_id" : 4
-        })
+        super().setUpClass()
+        cls.property = cls.env["property"].create(
+            {
+                "name": "Big Villa",
+                "state": "sold",
+                "description": "A nice and big villa",
+                "postcode": 12345,
+                "date_availability": "2023-05-02",
+                "expected_price": 1600000,
+                "bedrooms": 6,
+                "living_area": "100",
+                "facades": "4",
+                "garage": "True",
+                "garden": "True",
+                "garden_area": 10000,
+                "garden_orientation": "south",
+                "property_type_id": 4,
+            }
+        )
 
     def test_create_offer_with_sold_property(self):
         with self.assertRaises(ValidationError) as context:
-            self.env['property.offer'].create({
-                'property_id' : self.property.id,
-                'partner_id': '1',
-                'price': 1600000
-            })
+            self.env["property.offer"].create({"property_id": self.property.id, "partner_id": "1", "price": 1600000})
 
         self.assertIn("You can't add an offer to a sold property", str(context.exception))
-
